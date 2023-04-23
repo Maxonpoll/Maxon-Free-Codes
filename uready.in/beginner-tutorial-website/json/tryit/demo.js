0ec8be5811e67760a4f3a89f7b18a0acaac960ed
Maxon Poll <61685418+Maxonpoll@users.noreply.github.com>
2023-04-23 03:02:15 -0700

    maxon

---
+++ uready.in/beginner-tutorial-website/json/tryit/demo.js
@@ -0,0 +1,22 @@
+myFunction([
+{
+"display": "HTML Tutorial",
+"url": "http://www.vetripandi.vetbossel.in/html/index.html"
+},
+{
+"display": "CSS Tutorial",
+"url": "http://www.vetripandi.vetbossel.in/css/index.html"
+},
+{
+"display": "JavaScript Tutorial",
+"url": "http://www.vetripandi.vetbossel.in/javascript/index.html"
+},
+{
+"display": "jQuery Tutorial",
+"url": "http://www.vetripandi.vetbossel.in/jquery/index.html"
+},
+{
+"display": "JSON Tutorial",
+"url": "http://www.vetripandi.vetbossel.in/json/index.html"
+},
+])
